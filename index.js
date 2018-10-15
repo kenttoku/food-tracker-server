a4dd2f6c4ef8ad8a5fc5533763a227fbf6ec4586
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-15 09:20:26 -0400

    Start basic express app setup

---
+++ index.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const { PORT, CLIENT_ORIGIN, MONGODB_URI } = require('./config');
+const app = express();
+
+// Middleware
+app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'));
+app.use(cors({ origin: CLIENT_ORIGIN }));
+app.use(express.json());
+if (require.main === module) {
+  mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+    .then(() => console.log('Database Connected'))
+    .catch(err => console.error(err));
+
+  app.listen(PORT, function() {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => console.error(err));
+}
+
+module.exports = { app };
