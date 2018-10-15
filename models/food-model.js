cdfa706243c43c4d1829038045643603d6ed726b
Kent Tokunaga <kenttokunaga@gmail.com>
2018-10-15 10:31:13 -0400

    Create food model and food router

---
+++ models/food-model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const foodSchema = new mongoose.Schema({
+  name: String,
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  fruit: { type: Number, default: 0 },
+  vegetable: { type: Number, default: 0 },
+  wholeGrain: { type: Number, default: 0 },
+  leanProteins: { type: Number, default: 0 },
+  nutsAndSeeds: { type: Number, default: 0 },
+  dairy: { type: Number, default: 0 },
+  refinedGrain: { type: Number, default: 0 },
+  fattyProtein: { type: Number, default: 0 },
+  sweets: { type: Number, default: 0 },
+  friedFood: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model('Food', foodSchema);
---
+++ routers/food-router.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+
+const Food = require('../models/food-model');
+
+router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
+
+
+/* ========== GET/READ ALL ITEMS ========== */
+router.get('/', (req, res, next) => {
+  return 'all items';
+});
+
+/* ========== GET/READ A SINGLE ITEM ========== */
+router.get('/:id', (req, res, next) => {
+  return 'one item';
+});
+
+/* ========== POST/CREATE AN ITEM ========== */
+router.post('/', (req, res, next) => {
+  return 'item created';
+});
+
+/* ========== PUT/UPDATE A SINGLE ITEM ========== */
+router.put('/:id', (req, res, next) => {
+  return 'item updated';
+});
+
+/* ========== DELETE/REMOVE A SINGLE ITEM ========== */
+router.delete('/:id', (req, res, next) => {
+  return 'item deleted';
+});
+
+module.exports = router;
