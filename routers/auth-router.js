398ba9648c45908e464c4c583ed6daf8896d4ad5
Kent Tokunaga <kenttokunaga@gmail.com>
2018-12-28 11:58:14 -0500

    Create demo endpoint

--- routers/auth-router.js
+++ routers/auth-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const passport = require('passport');
 const createAuthToken = require('../utils/token');
+const User = require('../models/user-model');
 
 const router = express.Router();
 const options = { session: false, failWithError: true };
@@ -15,6 +16,14 @@ router.post('/login', localAuth, (req, res) => {
 router.post('/refresh', jwtAuth, (req, res) => {
   const authToken = createAuthToken(req.user);
   return res.json({ authToken });
+});
+
+router.post('/demo', (req, res) => {
+  User.findOne({ username: 'demouser' })
+    .then(user => {
+      const authToken = createAuthToken(user.serialize());
+      return res.json({ authToken });
+    });
 });
 
 module.exports = router;
